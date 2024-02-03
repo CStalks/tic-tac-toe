584f6c5ca60160073a515bb9b28397b975453e44
CStalks <swiafe.dev@gmail.com>
2024-02-03 11:54:27 -0500

    Complete cell function

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function GameBoard(){
+  let gameboard = [];
+}
+
+function Cell(){
+  let marker = 0;
+
+  const changeMarker = (playerMarker) => mark = playerMarker; 
+  const getMarker = () => value;
+
+  return {changeMarker, getMarker}
+}
+
+function GameController(playerOneName = "PlayerOne", playerOneName = "PlayerTwo"){
+
+
+}
+
