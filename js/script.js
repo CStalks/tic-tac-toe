587a76f199e76173cdac393cf93f5b1f05abadd5
CStalks <swiafe.dev@gmail.com>
2024-02-03 19:59:37 -0500

    Complete GameBoard function

--- js/script.js
+++ js/script.js
@@ -1,18 +1,49 @@
 function GameBoard(){
-  let gameboard = [];
+  const row = 3
+  const col = 3
+  let board = [];
+
+  for(let i = 0; i < row; i++){
+    board[i] = [];
+    for(let j = 0; j < col; j++){
+      board[i].push(Cell());
+    }
+  }
+
+  //for UI to render board
+  const getBoard = () => board;
+
+  const addMarker = (posX, posY, playerOption) => {
+    const cellAvailable = board[posX][posY].getMarker() === 0;
+    
+    if(!cellAvailable) return;
+    board[posX][posY].changeMarker() = playerOption;
+  }
+  
+   //a method to show the board to the console after each turn that we play
+   const printBoard = () => {
+    boardWithCellValues = board.map(row => row.filter(cell => cell.getMarker())); 
+    console.log(boardWithCellValues);
+  }
+  return {getBoard, addMarker, printBoard };
 }
 
+let here = GameBoard();
+
+// marker represents the symbols on the board
+// 0 represents no marker at that postion
+// 1 represents the symbol X at that positon
+// 2 represents the symbol O  at that position
 function Cell(){
   let marker = 0;
 
-  const changeMarker = (playerMarker) => mark = playerMarker; 
-  const getMarker = () => value;
+  const changeMarker = (playerMarker) => marker = playerMarker; 
+  const getMarker = () => marker;
 
-  return {changeMarker, getMarker}
+  return {changeMarker, getMarker};
 }
 
-function GameController(playerOneName = "PlayerOne", playerOneName = "PlayerTwo"){
-
-
+function GameController(playerOneName="playerOne", playerTwoName="playerTwoName"){
+  return {};
 }
 
