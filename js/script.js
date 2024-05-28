b71ee132e96d13e34552e56682161962be1126d4
CStalks <swiafe.dev@gmail.com>
2024-05-27 20:25:15 -0400

    Fix winner bug

--- js/script.js
+++ js/script.js
@@ -23,11 +23,9 @@ function GameBoard(){
   //a method to show the board to the console after each turn that we play
   const printBoard = () => {
     boardWithCellValues = board.map(row => row.map(cell => cell.getMarker())); 
-    console.log(boardWithCellValues);
   }
   return {getBoard, addMarker, printBoard};
 }
-
 
 // marker represents the symbols on the board
 // '' represents no marker at that postion
@@ -42,10 +40,34 @@ function Cell(){
   return {changeMarker, getMarker};
 }
 
-function GameController(playerOneName="playerOne", playerTwoName="playerTwoName"){
+function GameController(playerOneName="P1", playerTwoName="P2"){
   const board = GameBoard();
   const players = [{name: playerOneName, token: 'X'},{name:playerTwoName, token: 'O'}];
   let activePlayer = players[0];
+
+  const checkWin = () => {
+    let updatedBoard = board.getBoard();
+    
+    if(( updatedBoard[0][0].getMarker() === 'X' && updatedBoard[0][1].getMarker() === 'X' && updatedBoard[0][2].getMarker() === 'X')    ||
+        (updatedBoard[1][0].getMarker() === 'X' && updatedBoard[1][1].getMarker() === 'X' && updatedBoard[1][2].getMarker() === 'X')    ||
+        (updatedBoard[2][0].getMarker() === 'X' && updatedBoard[2][1].getMarker() === 'X' && updatedBoard[2][2].getMarker() === 'X')    ||
+        (updatedBoard[0][0].getMarker() === 'X' && updatedBoard[1][0].getMarker() === 'X' && updatedBoard[2][0].getMarker() === 'X')    ||
+        (updatedBoard[0][1].getMarker() === 'X' && updatedBoard[1][1].getMarker() === 'X' && updatedBoard[2][1].getMarker() === 'X')    ||
+        (updatedBoard[0][2].getMarker() === 'X' && updatedBoard[1][2].getMarker() === 'X' && updatedBoard[2][2].getMarker() === 'X')    ||
+        (updatedBoard[0][0].getMarker() === 'X' && updatedBoard[1][1].getMarker() === 'X' && updatedBoard[2][2].getMarker() === 'X')    ||
+        (updatedBoard[0][2].getMarker() === 'X' && updatedBoard[1][1].getMarker() === 'X' && updatedBoard[2][0].getMarker() === 'X')){
+         return true;
+    }else if((updatedBoard[0][0].getMarker() === 'O' && updatedBoard[0][1].getMarker() === 'O' && updatedBoard[0][2].getMarker() === 'O') ||
+        (updatedBoard[1][0].getMarker() === 'O' && updatedBoard[1][1].getMarker() === 'O' && updatedBoard[1][2].getMarker() === 'O')    ||
+        (updatedBoard[2][0].getMarker() === 'O' && updatedBoard[2][1].getMarker() === 'O' && updatedBoard[2][2].getMarker() === 'O')    ||
+        (updatedBoard[0][0].getMarker() === 'O' && updatedBoard[1][0].getMarker() === 'O' && updatedBoard[2][0].getMarker() === 'O')    ||
+        (updatedBoard[0][1].getMarker() === 'O' && updatedBoard[1][1].getMarker() === 'O' && updatedBoard[2][1].getMarker() === 'O')    ||
+        (updatedBoard[0][2].getMarker() === 'O' && updatedBoard[1][2].getMarker() === 'O' && updatedBoard[2][2].getMarker() === 'O')    ||
+        (updatedBoard[0][0].getMarker() === 'O' && updatedBoard[1][1].getMarker() === 'O' && updatedBoard[2][2].getMarker() === 'O')    ||
+        (updatedBoard[0][2].getMarker() === 'O' && updatedBoard[1][1].getMarker() === 'O' && updatedBoard[2][0].getMarker() === 'O')){
+          return true;
+    }
+  }
 
   const switchPlayerTurn = () => {
     activePlayer = activePlayer === players[0] ? players[1] : players[0];
@@ -55,39 +77,11 @@ function GameController(playerOneName="playerOne", playerTwoName="playerTwoName"
 
   const printNewRound = () => {
     board.printBoard();
-    console.log(`${getActivePlayer().name}'s turn.`);
-  }
-
-  const checkWin = () => {
-    let updatedBoard = board.getBoard();
-    
-    if( (updatedBoard[0][0].getMarker() === 'X' && updatedBoard[0][1].getMarker() === 'X' && updatedBoard[0][2].getMarker() === 'X')    ||
-        (updatedBoard[1][0].getMarker() === 'X' && updatedBoard[1][1].getMarker() === 'X' && updatedBoard[1][2].getMarker() === 'X')    ||
-        (updatedBoard[2][0].getMarker() === 'X' && updatedBoard[2][1].getMarker() === 'X' && updatedBoard[2][2].getMarker() === 'X')    ||
-        (updatedBoard[0][0].getMarker() === 'X' && updatedBoard[1][0].getMarker() === 'X' && updatedBoard[2][0].getMarker() === 'X')    ||
-        (updatedBoard[0][1].getMarker() === 'X' && updatedBoard[1][1].getMarker() === 'X' && updatedBoard[2][1].getMarker() === 'X')    ||
-        (updatedBoard[0][2].getMarker() === 'X' && updatedBoard[1][2].getMarker() === 'X' && updatedBoard[2][2].getMarker() === 'X')    ||
-        (updatedBoard[0][0].getMarker() === 'X' && updatedBoard[1][1].getMarker() === 'X' && updatedBoard[2][2].getMarker() === 'X')    ||
-        (updatedBoard[0][2].getMarker() === 'X' && updatedBoard[1][1].getMarker() === 'X' && updatedBoard[2][0].getMarker() === 'X')){
-         return `${getActivePlayer().name} Won`;
-    } else if((updatedBoard[0][0].getMarker() === 'O' && updatedBoard[0][1].getMarker() === 'O' && updatedBoard[0][2].getMarker() === 'O') ||
-        (updatedBoard[1][0].getMarker() === 'O' && updatedBoard[1][1].getMarker() === 'O' && updatedBoard[1][2].getMarker() === 'O')    ||
-        (updatedBoard[2][0].getMarker() === 'O' && updatedBoard[2][1].getMarker() === 'O' && updatedBoard[2][2].getMarker() === 'O')    ||
-        (updatedBoard[0][0].getMarker() === 'O' && updatedBoard[1][0].getMarker() === 'O' && updatedBoard[2][0].getMarker() === 'O')    ||
-        (updatedBoard[0][1].getMarker() === 'O' && updatedBoard[1][1].getMarker() === 'O' && updatedBoard[2][1].getMarker() === 'O')    ||
-        (updatedBoard[0][2].getMarker() === 'O' && updatedBoard[1][2].getMarker() === 'O' && updatedBoard[2][2].getMarker() === 'O')    ||
-        (updatedBoard[0][0].getMarker() === 'O' && updatedBoard[1][1].getMarker() === 'O' && updatedBoard[2][2].getMarker() === 'O')    ||
-        (updatedBoard[0][2].getMarker() === 'O' && updatedBoard[1][1].getMarker() === 'O' && updatedBoard[2][0].getMarker() === 'O')){
-          return `${getActivePlayer().name} Won`;
-    } else {
-      return false;
-    }
   }
 
   const playRound = (row, col) => {
-      if(checkWin()){
-        return;
-      }
+      if(checkWin()) return;
+
       board.addMarker(row, col, getActivePlayer().token);
       switchPlayerTurn();
       printNewRound();
@@ -95,7 +89,6 @@ function GameController(playerOneName="playerOne", playerTwoName="playerTwoName"
 
   // Initial game message
   printNewRound();
-
   return {playRound, getActivePlayer, getBoard: board.getBoard, checkWin};
 }
 
@@ -109,14 +102,14 @@ function ScreenController() {
 
     const board = game.getBoard();
     const activePlayer = game.getActivePlayer();
+    //checks the active player before we switched players
+    const winner = activePlayer.name === 'P1'? 'P2' : 'P1';
 
     if(game.checkWin()){
-      playerTurnDiv.textContent = `${activePlayer.name} Won`;
+      playerTurnDiv.textContent = `${winner} Won`;
     } else {
-      playerTurnDiv.textContent = `${activePlayer.name}'s turn...`;
-
+      playerTurnDiv.textContent = `${activePlayer.name}'s turn`;
     }
-
 
     // Render board squares 
     board.forEach((row, rowIndex) => {
